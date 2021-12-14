62f0d0a68051a5c2e14c627467022a7c59a76b29
fillipe.felix <felipesoares_1993@hotmail.com>
2021-12-14 14:27:30 -0300

    criação da interface e implementação de USER e suas viewModels

---
+++ DevFreela.Application/Services/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using DevFreela.Application.ViewModels;
+
+namespace DevFreela.Application.Services.Interfaces
+{
+    public interface IUserService
+    {
+        UserViewModel GetUser(int id);
+        int Create(CreateUserInputModel inputModel);
+    }
+}
