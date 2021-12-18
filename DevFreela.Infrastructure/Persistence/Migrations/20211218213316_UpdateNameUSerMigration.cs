1ad180d9a1db712169b68906fe114c4ea3081f41
fillipe.felix <felipesoares_1993@hotmail.com>
2021-12-18 18:35:23 -0300

    Correção de nome de atributo, criando a migration e fazendo o update

---
+++ DevFreela.Infrastructure/Persistence/Migrations/20211218213316_UpdateNameUSerMigration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevFreela.Infrastructure.Persistence.Migrations
+{
+    public partial class UpdateNameUSerMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Users",
+                newName: "FullName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Users",
+                newName: "Name");
+        }
+    }
+}
