8108549271cf82ac8995617715ca0c37869d2fb0
fillipe.felix <felipesoares_1993@hotmail.com>
2021-12-24 21:34:23 -0300

    Criando o primeiro teste do projeto

---
+++ DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,29 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+using Xunit;
+
+namespace DevFreela.UnitTests.Core.Entities
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void ProjectIfProjectStartWorks()
+        {
+            var project = new Project("Nome de Teste", "Descrição de Teste", 1, 2, 10000);
+
+            Assert.Equal(ProjectStatus.Created, project.Status);
+            Assert.Null(project.StartedAt);
+            
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+            
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+            
+            project.Start();
+
+            Assert.Equal(ProjectStatus.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}
